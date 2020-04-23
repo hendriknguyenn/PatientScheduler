9404e8abd33720941987ac9e256681558847c283
Michael Than <60087485+michaelthan24@users.noreply.github.com>
2020-04-22 21:13:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-package application;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
@@ -9,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -26,7 +26,7 @@ import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
     private Stage primaryStage;
     private TextField usernameTF; 
     private PasswordField pwTF;
@@ -47,7 +47,10 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         Label enterLoginInfoLb = new Label("Enter Login Credentials");
         Label userLb = new Label("Username: ");
         Label pwLb = new Label("Password: ");
-
+       
+        ChoiceBox<String> accountTypeDrop = new ChoiceBox<>();
+        accountTypeDrop.getItems().addAll("Receptionist", "Doctor", "Medical Employee");
+        
         LoginTextFieldListener textFieldListener = new LoginTextFieldListener();
         usernameTF = new TextField();
         pwTF = new PasswordField();
@@ -61,9 +64,9 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         gpLogin.setVgap(10);
         gpLogin.setHgap(10);
         gpLogin.setAlignment(Pos.CENTER);
-        VBox vbLogin = new VBox(20, enterLoginInfoLb, gpLogin, loginBtn);
+        VBox vbLogin = new VBox(20, enterLoginInfoLb, gpLogin, accountTypeDrop, loginBtn);
         vbLogin.setAlignment(Pos.CENTER);
-        loginBtn.setOnAction(this);
+        loginBtn.setOnAction(e -> handle(accountTypeDrop));
         return new Scene(vbLogin, 475, 375);
     }
     
@@ -71,7 +74,7 @@ public class Main extends Application implements EventHandler<ActionEvent> {
      * Patient schedule scene displays the weekly schedule
      */
     public Scene scheduleDisplay () {	
-    	Label patientSchedule = new Label("Patient Scheduler"); //only for testing
+    	Label patientSchedule = new Label("Patient Scheduler"); //only for testing, should be changed depending on different users.
     	VBox layout = new VBox(20, patientSchedule);
     	layout.setAlignment(Pos.CENTER);
     	return new Scene (layout, 600, 500);
@@ -87,22 +90,42 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             loginBtn.setDisable(usernameVal.trim().equals("") || passwordVal.trim().equals(""));
         }
     }
-    @Override
+    
     /*
-     * @param takes in the action that the user performs
-     * Desc: whenu user presses login button, the application will make connection to database to confirm credentials
+     * @param takes in the drop down menu option
+     * Desc: when user presses login button, the application will make connection to database to confirm credentials
      */
-    public void handle (ActionEvent event) { // when the button is clicked
-    	if(event.getSource() == loginBtn ) {
+    public void handle (ChoiceBox<String> accountTypeDrop ) { // when the button is clicked
     		try { 
-    			int ID = Integer.parseInt(usernameTF.getText());
+    			String credential = accountTypeDrop.getValue();	// getting the option the user chose in dropdown
+    			String IDquery = null;
+    			String passQuery = null;
+    			if(credential=="Receptionist") {				// changes values for obtaining login from msaccess database
+    				IDquery = "Receptionist_ID=?";
+    				passQuery = "R_Password=?";
+    			}
+    			else if (credential=="Doctor") {
+    				IDquery = "Doctor_ID=?";
+    				passQuery = "D_Password=?";
+    			}
+    			else if (credential=="Medical Employee") {
+    				credential = "MedicalEmployee";
+    				IDquery = "Med_Employee_ID=?";
+    				passQuery = "ME_Password=?";
+    			}
+    			int ID = Integer.parseInt(usernameTF.getText());  // get the ID number from the input
     			String pword = pwTF.getText();
-				Connection con = DriverManager.getConnection(dbUrl);
-				PreparedStatement pst = con.prepareStatement("Select * FROM Receptionist WHERE Receptionist_ID=? AND R_Password=?");
+    			Connection con = DriverManager.getConnection(dbUrl);
+				PreparedStatement pst = con.prepareStatement("Select * FROM " +credential+ " WHERE " +IDquery+ " AND " +passQuery);
 				pst.setInt(1, ID);
 				pst.setString(2,pword);
 				ResultSet rs = pst.executeQuery();
-				if (rs.next()) {
+				if (rs.next()) {			
+					/*
+					 * ADD if statements to open specific schedules for different users; for example if it is doctor, open the 
+					 * schedule so that it is the correct schedule for the role of the doctor to view. Doctors only get 
+					 * to view the schedule. EX: primaryStage.setScene(scheduleDisplayDoctor)
+					 */
 					primaryStage.setScene(scheduleDisplay());
 				} 
 				else {
@@ -118,7 +141,6 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 				invalidUser.setContentText("Please contact your administrator if you forgot your credentials");
 				invalidUser.showAndWait();
     		}
-    	}
     }
     /**
      * @wbp.parser.entryPoint
